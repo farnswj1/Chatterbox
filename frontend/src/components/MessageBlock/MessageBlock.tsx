751ce04b1588fba1276f77cf3e38a9047e408c3b
Justin Farnsworth <farnswj1@tcnj.edu>
2024-06-23 17:29:30 -0400

    improved UI responsiveness

--- frontend/src/components/MessageBlock/MessageBlock.tsx
+++ frontend/src/components/MessageBlock/MessageBlock.tsx
@@ -1,11 +1,25 @@
 import { FC } from 'react';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 import { ChatMessage } from 'types';
 
 type MessageBlockProps = ChatMessage;
 
 const MessageBlock: FC<MessageBlockProps> = ({ author, message }) => (
   <div className="mb-4">
-    <p><span className="font-bold">{author}</span>: {message}</p>
+    <p><span className="font-bold text-xl">{author}</span>:</p>
+    <ReactMarkdown
+      remarkPlugins={[remarkGfm]}
+      components={{
+        'div': ({ node, ...props }) => <div className="mb-4" {...props} />,
+        'p': ({ node, ...props }) => <p className="mb-4" {...props} />,
+        'ul': ({ node, ...props }) => <ul className="mb-4" {...props} />,
+        'ol': ({ node, ...props }) => <ol className="mb-4" {...props} />,
+        'li': ({ node, ...props }) => <li className="mb-4" {...props} />
+      }}
+    >
+      {message}
+    </ReactMarkdown>
   </div>
 );
 
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -38,37 +38,41 @@ const Home: FC = () => {
   };
 
   return (
-    <main className="px-4 container mx-auto h-full max-w-3xl flex flex-col justify-between">
-      <div>
-        {
-          chatMessages.map((chatMessage, index) => (
-            <MessageBlock key={index} {...chatMessage} />
-          ))
-        }
-        {
-          response.length > 0 && (
-            <MessageBlock author="Ollama" message={response} />
-          )
-        }
+    <main className="px-4">
+      <div className="container mx-auto min-h-[calc(100vh-128px)] max-w-3xl flex flex-col justify-between">
+        <div>
+          {
+            chatMessages.map((chatMessage, index) => (
+              <MessageBlock key={index} {...chatMessage} />
+            ))
+          }
+          {
+            response.length > 0 && (
+              <MessageBlock author="Ollama" message={response} />
+            )
+          }
+        </div>
+        <form className="flex gap-4" onSubmit={handleSubmit}>
+          <textarea
+            name="message"
+            placeholder="Send a message."
+            className="border rounded p-2 dark:bg-inherit"
+            rows={1}
+            cols={80}
+            disabled={disabled}
+            required
+          />
+          <div>
+            <button
+              className="border rounded p-2 dark:bg-inherit"
+              type="submit"
+              disabled={disabled}
+            >
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
-      <form className="flex gap-4" onSubmit={handleSubmit}>
-        <textarea
-          name="message"
-          placeholder="Send a message."
-          className="rounded p-2"
-          rows={1}
-          cols={80}
-          disabled={disabled}
-          required
-        />
-        <button
-          className="border rounded p-2"
-          type="submit"
-          disabled={disabled}
-        >
-          Submit
-        </button>
-      </form>
     </main>
   );
 };
